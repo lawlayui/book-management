9214a6120c7c84ace612d4274a566e375caee64a
lawlayui82 <lawlayui82@gmail.com>
2026-06-05 18:07:02 +0700

    wip: add BooksRepository for acces database

---
+++ src/main/java/learn/example/database_migration/repository/BooksRepository.java
@@ -0,0 +1,13 @@
+package learn.example.database_migration.repository;
+
+
+import org.springframework.boot.data.autoconfigure.web.DataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import learn.example.database_migration.entity.Book;
+
+public interface BooksRepository extends JpaRepository<Book, Long>{
+    Page<Book> findByAuthor(String author, Pageable pageable);
+    Page<Book> findByTitle(String title, Pageable pageable);
+}
