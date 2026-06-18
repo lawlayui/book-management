14cf3fbb2db335e6604f0f7c2f99e7ea92eb9838
lawlayui82 <lawlayui82@gmail.com>
2026-06-18 07:27:54 +0700

    Add NoArgsConstructor annotation to BaseDTO class

--- src/main/java/com/lawlayui/library/api/dto/BaseDTO.java
+++ src/main/java/com/lawlayui/library/api/dto/BaseDTO.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
@@ -11,6 +12,7 @@ import java.time.Instant;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class BaseDTO<ID> {
     protected ID id;
     protected Instant timestamp;
